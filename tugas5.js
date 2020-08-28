156a552ce5866a7680619470240a3047ccd31282
folksfolk <dhaniiihh@gmail.com>
2020-08-28 22:36:47 +0700

    tambah tugas

---
+++ tugas5.js
@@ -0,0 +1,59 @@
+// Jawaban Soal Nomor 1
+
+function halo() {
+    return "Halo Sanbers!";
+  }
+  
+console.log(halo())
+
+// Jawaban Soal Nomor 2
+
+function kalikan(num1, num2) {
+    return num1 * num2;
+}
+  
+var num1 = 12
+var num2 = 4
+   
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali)
+
+// Jawaban Soal Nomor 3
+
+function introduce(name, age, address, hobby) {
+    return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di "
+    + address + ", dan saya punya hobi " + hobby + "!";
+}
+  
+var name = "John"
+var age = 30
+var address = "Jalan belum jadi"
+var hobby = "Gaming"
+   
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan)
+
+// Jawaban Soal Nomor 4
+
+var arrayDaftarPeserta = ["Asep", "laki-laki", "baca buku" , 1992]
+var pesertaObj = {
+  nama: "Asep",
+  JenisKelamin: "laki-laki",
+  hobi: "baca buku",
+  Tahun_lahir: 1992
+}
+
+console.log(pesertaObj.nama)
+console.log(pesertaObj.JenisKelamin)
+console.log(pesertaObj.hobi)
+console.log(pesertaObj.Tahun_lahir)
+
+// Jawaban Soal Nomor 5
+
+var buah = [{nama: "strawberry", warna: "merah", adaBijinya: "tidak", harga: 9000},
+           {nama: "jeruk", warna: "oranye", adaBijinya: "ada", harga: 8000},
+           {nama: "Semangka", warna: "Hijau dan Merah", adaBijinya: "ada",harga: 10000},
+           {nama: "Pisang", warna: "Kuning", adaBijinya: "tidak", harga: 5000}]
+
+console.log(buah[0])
+// Jawaban Soal Nomor 6
